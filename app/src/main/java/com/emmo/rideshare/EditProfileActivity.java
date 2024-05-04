12791b8050bf6a59fc65fb9dbc44b48e731a16c9
molitor <emmo1001@stud.hs-kl.de>
2024-05-04 15:59:28 +0200

    Authentification einfügen

--- app/src/main/java/com/emmo/rideshare/EditProfileActivity.java
+++ app/src/main/java/com/emmo/rideshare/EditProfileActivity.java
@@ -1,6 +1,9 @@
 package com.emmo.rideshare;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class EditProfileActivity extends AppCompatActivity {
+    private EditText vname;
+    private EditText nname;
+    private EditText email;
+    private EditText street;
+    private EditText streetnr;
+    private EditText zip;
+    private EditText city;
+    private Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,24 @@ public class EditProfileActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        vname = findViewById(R.id.profile_vname);
+        nname = findViewById(R.id.profile_nname);
+        email = findViewById(R.id.profile_email);
+        street = findViewById(R.id.profile_address_str);
+        streetnr = findViewById(R.id.profile_address_nr);
+        zip = findViewById(R.id.profile_address_plz);
+        city = findViewById(R.id.profile_address_city);
+        save = findViewById(R.id.update_btn);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void loadUser(){
+
     }
 }
---
+++ app/src/main/java/com/emmo/rideshare/EmailPasswordActivity.java
@@ -0,0 +1,68 @@
+package com.emmo.rideshare;
+
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class EmailPasswordActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    public void initializeUser(){
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            //reload();
+        }
+    }
+
+    public void createUser(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        //updateUI(user);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(EmailPasswordActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(EmailPasswordActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        //updateUI(null);
+                    }
+                });
+    }
+
+    public void signInUser(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        //updateUI(user);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        Toast.makeText(EmailPasswordActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        //updateUI(null);
+                    }
+                });
+    }
+
+    public void signOut(){
+        FirebaseAuth.getInstance().signOut();
+    }
+
+}
